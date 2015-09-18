9024ea67fc46c43235c8805c69a57eef821c5a3c
Sujay Khandekar <sskhandek@gmail.com>
2015-09-18 17:46:41 -0500

    Adding in Handlebars

--- slackoff.js
+++ slackoff.js
@@ -1,3 +1,4 @@
+// Client Code
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
@@ -9,13 +10,36 @@ if (Meteor.isClient) {
   });
 
   Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+    'keypress input': function(event) {
+      if (event.charCode == 13) { // Enter Keystroke
+        event.stopPropagation();
+        $('.message-history')
+            .append(
+              '<div class="message">' +
+              '<a href="" class="message_profile-pic"></a>' +
+              '<a href="" class="message_username">scotch</a>' +
+              '<span class="message_timestamp">1:31 AM</span>' +
+              '<span class="message_star"></span>' +
+              '<span class="message_content">' +
+              $('.input-box_text').val() +
+              '</span>' +
+              '</div>');
+        $('.input-box_text').val("");
+        return false;
+      }
     }
+  });
+
+  Template.messages.helpers({
+    messages: [
+      {text: "All these messages."},
+      {text: "Sujay is ACM chair"},
+      {text: "Meteor is cool"}
+    ]
   });
 }
 
+// Server Code
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
