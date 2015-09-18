8e970825d86564fb52a61616522c6cc2c094b8fa
Sujay Khandekar <sskhandek@gmail.com>
2015-09-18 18:48:16 -0500

    Finished First part

--- slackoff.js
+++ slackoff.js
@@ -9,21 +9,20 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.hello.events({
+  Template.footer.events({
     'keypress input': function(event) {
-      if (event.charCode == 13) { // Enter Keystroke
+      if (event.charCode == 13) { // Enter key
         event.stopPropagation();
         $('.message-history')
-            .append(
-              '<div class="message">' +
+            .append('<div class="message">' +
               '<a href="" class="message_profile-pic"></a>' +
               '<a href="" class="message_username">scotch</a>' +
               '<span class="message_timestamp">1:31 AM</span>' +
               '<span class="message_star"></span>' +
               '<span class="message_content">' +
               $('.input-box_text').val() +
-              '</span>' +
-              '</div>');
+              '</span></div>'
+            );
         $('.input-box_text').val("");
         return false;
       }
