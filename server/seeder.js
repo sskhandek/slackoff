e84e1728cc1a4c4bd0ac08d46e359d55691c77d2
Sujay Khandekar <sskhandek@gmail.com>
2015-09-18 22:47:55 -0500

    Getting mongo setup

---
+++ server/seeder.js
@@ -0,0 +1,19 @@
+/**
+ * Created by SujayKhandekar on 9/18/15.
+ */
+Meteor.startup(function() {
+    Factory.define('message', Messages, {
+        text: function() {
+            return Fake.sentence();
+        }
+    });
+
+    // Add this if you want to remove all messages before seeding
+    Messages.remove({});
+
+    if (Messages.find({}).count() === 0) {
+        _(10).times(function(n) {
+            Factory.create('message');
+        });
+    }
+});
